28d1992d19971cf31103fa5ca6b5d58e2d62b754
Aman sharma <87026675+Aman300@users.noreply.github.com>
2024-07-08 12:17:56 +0530

    Update ludoController.js

--- controllers/ludoController.js
+++ controllers/ludoController.js
@@ -7,19 +7,20 @@ exports.root = (_req,res)=>{
     res.redirect(301,'/');
 }
 
-exports.room = (req,res)=>{
-    if(
-    Object.keys(rooms).includes(req.params.ROOMCODE) &&
-    Object.keys(req.query).length===0  &&
-    Object.keys(rooms[req.params.ROOMCODE]).length < 2 &&
+exports.room = (req, res) => {
+    const { token, game_id, ...otherParams } = req.query;
+    
+    if (
+        Object.keys(rooms).includes(req.params.ROOMCODE) &&
+        Object.keys(otherParams).length === 0 &&
+        Object.keys(rooms[req.params.ROOMCODE]).length < 2 &&
         (
-        !(NumberOfMembers[req.params.ROOMCODE].constant) ||
-        Object.keys(rooms[req.params.ROOMCODE]).length < NumberOfMembers[req.params.ROOMCODE].members
+            !(NumberOfMembers[req.params.ROOMCODE].constant) ||
+            Object.keys(rooms[req.params.ROOMCODE]).length < NumberOfMembers[req.params.ROOMCODE].members
         )
-    ){
+    ) {
         res.sendFile('ludo.html', { root: views });
-    } else{
-        res.statusCode = 404;
-        res.end('404!:(\nThis is either not a valid Room Code or The room is filled up, Go to home and create a room!');
+    } else {
+        res.status(404).send('404!:(\nThis is either not a valid Room Code or The room is filled up, Go to home and create a room!');
     }
 }
