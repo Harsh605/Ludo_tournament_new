c838e97a4bd65d2347bcfac49ad4e835459385d0
Aman sharma <87026675+Aman300@users.noreply.github.com>
2024-07-08 11:55:28 +0530

    Update ludoController.js

--- controllers/ludoController.js
+++ controllers/ludoController.js
@@ -7,21 +7,43 @@ exports.root = (_req,res)=>{
     res.redirect(301,'/');
 }
 
-exports.room = (req,res)=>{
+// exports.room = (req,res)=>{
 
-    let ROOMCODE = req.query.room_code
-    if(
-    Object.keys(rooms).includes(ROOMCODE) &&
-    Object.keys(req.query).length===0  &&
-    Object.keys(rooms[ROOMCODE]).length < 2 &&
+//     let ROOMCODE = req.query.room_code
+//     if(
+//     Object.keys(rooms).includes(ROOMCODE) &&
+//     Object.keys(req.query).length===0  &&
+//     Object.keys(rooms[ROOMCODE]).length < 2 &&
+//         (
+//         !(NumberOfMembers[ROOMCODE].constant) ||
+//         Object.keys(rooms[ROOMCODE]).length < NumberOfMembers[ROOMCODE].members
+//         )
+//     ){
+//         res.sendFile('ludo.html', { root: views });
+//     } else{
+//         res.statusCode = 404;
+//         res.end('404!:(\nThis is either not a valid Room Code or The room is filled up, Go to home and create a room!');
+//     }
+// }
+
+exports.room = (req, res) => {
+    let ROOMCODE = req.query.room_code;
+    let token = req.query.token;
+    let game_id = req.query.game_id;
+
+    if (
+        Object.keys(rooms).includes(ROOMCODE) &&
+        Object.keys(rooms[ROOMCODE]).length < 2 &&
         (
-        !(NumberOfMembers[ROOMCODE].constant) ||
-        Object.keys(rooms[ROOMCODE]).length < NumberOfMembers[ROOMCODE].members
+            !(NumberOfMembers[ROOMCODE].constant) ||
+            Object.keys(rooms[ROOMCODE]).length < NumberOfMembers[ROOMCODE].members
         )
-    ){
+    ) {
+        // You might want to do something with token and game_id here
+        // For example, you could attach them to the response somehow
+
         res.sendFile('ludo.html', { root: views });
-    } else{
-        res.statusCode = 404;
-        res.end('404!:(\nThis is either not a valid Room Code or The room is filled up, Go to home and create a room!');
+    } else {
+        res.status(404).send('404!:(\nThis is either not a valid Room Code or The room is filled up, Go to home and create a room!');
     }
 }
