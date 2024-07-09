716c5c5865df7f4f99dd005e8989e122dd7b85b4
Aman sharma <87026675+Aman300@users.noreply.github.com>
2024-07-09 16:47:50 +0530

    Update ludo.js

--- public/js/ludo.js
+++ public/js/ludo.js
@@ -337,7 +337,7 @@ socket.on('disconnect', function(){
 });
 
 //Output the message through DOM manipulation
-async function outputMessage(anObject,k){
+function outputMessage(anObject,k){
     let msgBoard = document.querySelector('.msgBoard');
 
     if(k===1 && !(anObject.Name.includes('<') || anObject.Name.includes('>') || anObject.Name.includes('/'))){    
@@ -381,31 +381,34 @@ async function outputMessage(anObject,k){
         //     });
         // })
 
-        if (window.confirm("You are the winner of this game")) {
-            const headers = {
-                Authorization: `Bearer ${urlParams.get('token')}`,
-                'Content-Type': 'application/json' // Ensure the Content-Type header is set for JSON
-            };
+        alert("You are the winner of this game", {
+            
+            onClose: async () => {
+
+                const headers = {
+                    Authorization: `Bearer ${urlParams.get('token')}`,
+                    'Content-Type': 'application/json' // Ensure the Content-Type header is set for JSON
+                };
+
+                try {
+                    const response = await fetch(`http://84.247.133.7:5010/challange/result/${urlParams.get('game_id')}`, {
+                        method: 'POST',
+                        headers: headers,
+                        body: JSON.stringify({
+                            status: "winn"
+                        })
+                    });
         
-            try {
-                const response = await fetch(`http://84.247.133.7:5010/challange/result/${urlParams.get('game_id')}`, {
-                    method: 'POST',
-                    headers: headers,
-                    body: JSON.stringify({
-                        status: "winn"
-                    })
-                });
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
         
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
+                    window.location.reload();
+                } catch (e) {
+                   console.log(e)
                 }
-        
-                window.location.reload();
-            } catch (e) {
-                console.log(e);
             }
-        }
-        
+        });
         
 
         const div = document.createElement('div');
