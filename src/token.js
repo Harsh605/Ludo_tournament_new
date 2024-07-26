6653243a555f3fa69055e4b4e7aa80e853673f49
Aman sharma <87026675+Aman300@users.noreply.github.com>
2024-07-26 11:55:46 +0530

    update

--- src/token.js
+++ src/token.js
@@ -1,16 +1,5 @@
 export const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjEsInJvbGUiOiJhZG1pbiIsImlhdCI6MTY5MjgxMjMzNSwiZXhwIjoxNjk1NDA0MzM1fQ.VWmSahbjKAdNxlmVmlITfZQTXF3EsPWJcWy_W4t60Bo"
 
-// export const token2 = ""
 
-// export const baseURL = "http://backend.ludokavish.com/";
-// export const baseURL = "http://84.247.133.7:5010/";
-// export const baseURL = "http://localhost:5010/";
-// export const socketURL = "http://localhost:5010/";
-
-
-export const baseURL = "http://93.127.199.145:5010/";
-export const socketURL = "http://93.127.199.145:5010/";
-// export const baseURL = "https://misty-pelican.cyclic.cloud/api/v1";
-
-// export const baseURL = "https://backened.ludokavish.com/api/v1";
-// export const baseURL = "https://1t4f35fh-3000.inc1.devtunnels.ms/api/v1";
+export const baseURL = "https://api.ludowinners.in/";
+export const socketURL = "http://api.ludowinners.in/";
